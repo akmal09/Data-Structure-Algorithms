51e864c8078407ca36277e7f309bb9eb86277d1c
akmal09 <akmalfauzan.af@gmail.com>
2024-05-22 13:38:10 +0700

    Djikstra Builder On progress

--- src/TreeDataStructure/DjikstraAlgorithm/Djikstra.java
+++ src/TreeDataStructure/DjikstraAlgorithm/Djikstra.java
@@ -26,5 +26,27 @@ public class Djikstra {
 
         this.b.addEdge(new Edge(8, this.c));
         this.b.addEdge(new Edge(7, this.h));
+
+        this.c.addEdge(new Edge(7, this.d));
+        this.c.addEdge(new Edge(4, this.f));
+        this.c.addEdge(new Edge(2, this.i));
+
+        this.d.addEdge(new Edge(9, this.e));
+        this.d.addEdge(new Edge(14, this.f));
+
+        this.e.addEdge(new Edge(10, this.f));
+
+        this.f.addEdge(new Edge(14, this.d));
+        this.f.addEdge(new Edge(4, this.c));
+        this.f.addEdge(new Edge(2, this.g));
+
+        this.g.addEdge(new Edge(6, this.i));
+        this.g.addEdge(new Edge(1, this.h));
+
+        this.h.addEdge(new Edge(8, this.a));
+        this.h.addEdge(new Edge(11, this.b));
+        this.h.addEdge(new Edge(7, this.i));
+
+        this.i.addEdge(new Edge(2, this.c));
     }
 }
