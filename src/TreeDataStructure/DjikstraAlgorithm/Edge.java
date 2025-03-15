bdea428075f2d98eba715edd349ecf9ad6c85a77
akmal09 <akmalfauzan.af@gmail.com>
2025-03-15 15:24:32 +0700

    dijkstra adjustment

---
+++ src/TreeDataStructure/DjikstraAlgorithm/Edge.java
@@ -0,0 +1,17 @@
+package TreeDataStructure.DjikstraAlgorithm;
+
+public class Edge {
+    public int distance;
+
+    private Node node;
+
+    public Edge(int distance, Node node){
+        this.distance = distance;
+        this.node = node;
+    }
+
+    public Node getNode(){
+        return this.node;
+    }
+
+}
